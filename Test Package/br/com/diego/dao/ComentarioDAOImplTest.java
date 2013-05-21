c83da08b19740e48b8f7b91adb5f1bd5f715b79e
Diego <diego.litus@gmail.com>
2013-05-21 20:21:11 -0300

    Adicionando o arquivod xml "applicationContextTest" para teste, atualizando a biblioteca spring mvc 3.1.1 para 3.2.

--- Test Package/br/com/diego/dao/ComentarioDAOImplTest.java
+++ Test Package/br/com/diego/dao/ComentarioDAOImplTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author disou_000
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:WEB-INF/applicationContext.xml")
+@ContextConfiguration(locations = {"file:web/WEB-INF/applicationContextTest.xml"})
 public class ComentarioDAOImplTest {
     
     @Autowired
